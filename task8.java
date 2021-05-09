747c4a27f59982c9a8b6a518bd23455345cababe
swaylexx <78600867+swaylexx@users.noreply.github.com>
2021-05-10 00:42:03 +0300

    Add files via upload

---
+++ task8.java
@@ -0,0 +1,19 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class task8 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(getSha256Hash("password123"));
+    }
+
+    private static String getSha256Hash(String s) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashInBytes = md.digest(s.getBytes(StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashInBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
